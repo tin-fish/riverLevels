44427c62bac9da217503ad363252cc0b2b1d28e0
Martin Fisher <mafisher@splunk.com>
2017-08-15 13:21:53 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,6 @@
+# River Levels
+noddy script to pull river level readings in.   
+dat file held to ensure the script does not duplicate rows.
+
+the idea here is to get the full backfill from the EA and then
+top up that data with an hourly/daily poll of the metrics.
---
+++ getRiverLevel.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+import urllib2
+import re
+from dateutil import parser
+from datetime import datetime
+try:
+	m=open('prevRiverLevel.dat','r')
+	past = m.read().splitlines()
+	m.close()
+except:
+	past=[]
+url="https://flood-warning-information.service.gov.uk/station/7270"
+HTML = urllib2.urlopen(url)
+t = HTML.read().splitlines()
+thisreadingmetric=""
+for l in reversed(t):
+	readingdate=re.match("\s+<td scope=\"row\"><time datetime=\"([\dT.\-:]+)Z\">([\dT.\-:]+)Z<\/time><\/td>",l)
+	readingmetric=re.match("\s+<td class=\"numeric\">([\d.]+)<\/td>",l)
+	if readingmetric:
+		thisreadingmetric=readingmetric.group(1)
+	if readingdate:
+		thisreadingdate=readingdate.group(1)
+		thisreadingepoch=(parser.parse(thisreadingdate) - datetime(1970, 1, 1)).total_seconds()
+		line = thisreadingdate+" "+str(thisreadingepoch)+" "+str(thisreadingmetric)
+		if not line in past:
+			print line
+			m=open('prevRiverLevel.dat','a')
+			m.write(line+"\n")
+			m.close
